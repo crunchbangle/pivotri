ff03f52ca2c4b25a119841d4e454a793d2043ee0
crunchbangle <crunchbangle@tuta.io>
2026-04-25 09:26:37 +0100

    test plan

---
+++ try2/hexgrid.spec.ts
@@ -0,0 +1,13 @@
+import { Point, rotate, orderTriangles, getNeighbouringTriangles, distanceBetweenPoints } from "./hexgrid";
+import { describe, test, it, expect } from "@jest/globals";
+
+// TODO:
+
+// GridShape and HexGrid
+describe("hexgrid shape constructions with sizes", () => {});
+
+// rotate on HexGrid
+describe("hexgrid rotations", () => {}); // 
+
+// unit tests on the helper functions for rotation
+describe("hexgrid functions", () => {});
