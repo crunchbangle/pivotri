251c38e82f6cf7335d0369abf6307bc7eaddd843
crunchbangle <crunchbangle@tuta.io>
2026-04-25 09:22:34 +0100

    fleshing out hexgrid.ts a bit

--- try2/hexgrid.ts
+++ try2/hexgrid.ts
@@ -6,6 +6,65 @@ export type Point = {
     y: number;
 };
 
+export type RankDef = {
+    index: number;
+    offset: number;
+    files: number;
+}
+
+
+// lets separate grid construction/logic from scaling to pixels, etc
+// these classes are for constructing the grid on the drawing context
+export class GridShape {
+    // this allows for any shape of grid in the horizontal orientation,
+    // though we could just interpret it as being vertical too.
+    totalMUnits: number; // number of vertical units (2 rows of triangles) in the grid
+    totalNUnits: number; // number of horizontal units (columns of triangles) in the grid
+    // using units enforces functional shaping, though there could still be disconnected subgraphs
+    ranks: RankDef[]; // the ranks of the grid, where each rank is a horizontal slice of the grid
+    // (but could be interpreted as vertical if we wanted to)
+    constructor(totalMUnits: number, totalNUnits: number, ranks: RankDef[]) {
+        this.totalMUnits = totalMUnits;
+        this.totalNUnits = totalNUnits;
+        this.ranks = ranks;
+    }
+}
+
+export class HexGrid { 
+    gridShape: GridShape; // defines the shape in rank and file
+    interval: number; // distance between neighbouring nodes or triangle centres
+    nodeSize: number; // radius of the node circles
+    spacing: number; // distance between the edge of a triangle and the centre of a node
+
+    // the rest of the properties are calculated from the above parameters
+    
+    hSpacing: number; // calculated spacing between triangle centres in the horizontal direction
+    vSpacing: number; // calculated spacing between triangle centres in the vertical direction
+    gridWidth: number; // calculated width of the grid in terms of triangle centres
+    gridHeight: number; // calculated height of the grid in terms of triangle centres
+    totalWidth: number; // calculated total width of the grid in pixels
+    totalHeight: number; // calculated total height of the grid in pixels
+    vUnits: number; // number of vertical units (2 rows of triangles) in the grid
+    hUnits: number; // number of horizontal units (columns of triangles) in the grid
+    constructor(interval: number, spacing: number, nodeSize: number, gridShape: GridShape) {
+        this.vUnits = gridShape.totalMUnits;
+        this.hUnits = gridShape.totalNUnits;
+        this.gridShape = gridShape;
+        this.interval = interval;
+        this.spacing = spacing;
+        this.nodeSize = nodeSize;
+        this.hSpacing = interval + spacing*2/Math.sqrt(3);
+        this.vSpacing = interval*Math.sqrt(3)/2 + spacing;
+        this.gridWidth = this.hUnits * 2 - 1;
+        this.gridHeight = this.vUnits * 2;
+        this.totalWidth = this.gridWidth * this.hSpacing;
+        this.totalHeight = this.gridHeight * this.vSpacing;
+    }
+}
+
+// the following functions are for calculating rotations on graphical objects
+// they are independent of the above, but rely on the correct layout from above.
+
 export const rotate = (nodePoint: Point, trianglePoints: Point[], spacing: number, by: number=1): Point[] => {
     // this puts everything below together:
     const neighbours = getNeighbouringTriangles(nodePoint, trianglePoints, spacing);
